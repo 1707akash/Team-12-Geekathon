01962dc293d8916c9239be7b7dd9b5b8534d2ad9
karnatipavanteja <pavankarnati190@gmail.com>
2024-02-12 11:11:31 +0530

    cloned

---
+++ Pavan/script.js
@@ -0,0 +1,4 @@
+const typingGround=document.querySelector(selectors,'#textarea')
+const btn=document.querySelector(selectors,'.btn)
+const score =document.querySelector(selector,'#score')
+const showSentence=document.querySelector(selector,'#showSentence')
